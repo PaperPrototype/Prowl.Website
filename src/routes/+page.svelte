2cad958cae55522400a6ef4c462a0c2d728dc221
Abdiel Lopez <a.paperprototype@gmail.com>
2024-11-23 00:29:36 -0500

    added more features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:head>
-  <title>Prowl Engine</title>
+  <title>Prowl Game Engine</title>
   <meta name="title" content="Prowl Game Engine" />
   <meta name="description" content="An open-source Unity-like game engine" />
   <meta name="image" content="/landing.png" />
@@ -68,59 +68,75 @@
   <NewGrid />
 </div>
 
+<!-- FEATURES -->
 <div class="flex flex-col gap-24">
+  <!-- feature -->
   <div class="w-screen grid grid-cols-2 container mx-auto max-w-5xl">
+    <div class="p-5 my-auto">
+      <h2 class="text-4xl font-semibold pb-5">Unity Scripting API</h2>
+      <div class="flex flex-col gap-3">
+        <p class="text-white/70">
+          Featuring a familiar Unity scripting API and workflow, making the
+          transition easy.
+        </p>
+      </div>
+    </div>
+    <div class="aspect-square bg-red-500 w-[32rem]"></div>
+  </div>
+
+  <!-- feature -->
+  <div class="w-screen grid grid-cols-2 container mx-auto max-w-5xl">
+    <div class="aspect-square bg-red-500 w-[32rem]"></div>
     <div class="p-5 my-auto">
       <h2 class="text-4xl font-semibold pb-5">Doubles Support</h2>
       <div class="flex flex-col gap-3">
         <p class="text-white/70">
-          Efficiently render at any distance with camera relative rendering by
-          default.*
+          Double precision coordinates coupled with camera relative rendering by
+          default allows you to efficiently render at any distance.
         </p>
         <span class="text-white/70 text-xs"
-          >*Tested up to 50,000,000,000 meters.</span
+          >Tested up to 50,000,000,000 meters.</span
+        >
+      </div>
+    </div>
+  </div>
+
+  <!-- feature -->
+  <div class="w-screen grid grid-cols-2 container mx-auto max-w-5xl">
+    <div class="p-5 my-auto">
+      <h2 class="text-4xl font-semibold pb-5">Large Scale Physics</h2>
+      <div class="flex flex-col gap-3">
+        <p class="text-white/70">
+          Simulate a small solar system with double precision JitterPhysics.
+        </p>
+        <span class="text-white/70 text-xs"
+          >Our solar system fits in prowl if you scale it down by a factor of
+          10.</span
         >
       </div>
     </div>
     <div class="aspect-square bg-red-500 w-[32rem]"></div>
   </div>
 
+  <!-- feature -->
   <div class="w-screen grid grid-cols-2 container mx-auto max-w-5xl">
     <div class="aspect-square bg-red-500 w-[32rem]"></div>
     <div class="p-5 my-auto">
       <h2 class="text-4xl font-semibold pb-5">Large Scale Physics</h2>
       <div class="flex flex-col gap-3">
         <p class="text-white/70">
-          Simulate a small solar system with double precision JitterPhysics.**
+          Simulate a small solar system with double precision JitterPhysics!
         </p>
         <span class="text-white/70 text-xs"
-          >**Our solar system fits in prowl if you scale it down by a factor of
+          >Our solar system fits in prowl if you scale it down by a factor of
           10.</span
         >
       </div>
     </div>
   </div>
-
-  <div class="w-screen grid grid-cols-2 container mx-auto max-w-5xl">
-    <div class="p-5 my-auto">
-      <h2 class="text-4xl font-semibold pb-5">Doubles Support</h2>
-      <div class="flex flex-col gap-3">
-        <p class="text-white/70">
-          Efficiently render at any distance with camera relative rendering by
-          default.* You can now simulate physics for a small solar system thanks
-          to double precision physics with Jitter.**
-        </p>
-        <span class="text-white/70 text-xs"
-          >*Tested up to 50,000,000,000 meters. **Our solar system fits in prowl
-          if you scale it down by a factor of 10.</span
-        >
-      </div>
-    </div>
-    <div class="aspect-square bg-red-500 w-[32rem]"></div>
-  </div>
 </div>
 
 <div class="p-10 text-center text-xs">
-  <p class="text-white/50">Released under the MIT License</p>
+  <p class="text-white/50">Engine released under the MIT License</p>
   <p class="text-white/50">Copyright © 2022-present</p>
 </div>
